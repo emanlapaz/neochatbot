128babf352c72a900375ee1b1773687e8bae36e3
emanlapaz <eugeniojrmanlapaz@gmail.com>
2024-02-02 23:52:43 +0000

    voice chat fully functioning

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -61,6 +61,40 @@ function Controller() {
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        {/*chat: loop and iterates the messages*/}
+        <div className="mt-5 px-5">
+          {messages.map((audio, index) => {
+            return (
+              <div
+                key={index + audio.sender}
+                className={
+                  "flex flex-col " + (audio.sender == "neo" && "flex items-end")
+                }
+              >
+                {/*Sender*/}
+                <div className="mt-4">
+                  <p
+                    className={
+                      audio.sender == "neo"
+                        ? "text-right mr-2 italic text-green-500"
+                        : "ml-2 italic text-blue-500"
+                    }
+                  >
+                    {audio.sender}
+                  </p>
+
+                  {/*audio message*/}
+                  <audio
+                    src={audio.blobUrl}
+                    className="appearance-none"
+                    controls
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
         {/*recorder*/}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
           <div className="flex justify-center items-center w-full">
