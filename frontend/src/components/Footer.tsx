b8adef2f822d1bdfdf4363b098646548d29e0e4e
emanlapaz <eugeniojrmanlapaz@gmail.com>
2024-03-16 22:06:27 +0000

    updated footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,12 +4,26 @@ function Footer() {
   return (
     <div className="mt-auto p-20 bg-gradient-to-b from-transparent to-black text-white font-bold">
       <div className="flex space-x-4">
-        {/* Placeholder 1 */}
-        <div className="bg-gray-700 p-2 rounded">GitHub Repo</div>
+        {/* GitHub Repo Link */}
+        <a
+          href="https://github.com/emanlapaz/neochatbot"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-700 p-2 rounded"
+        >
+          GitHub Repo
+        </a>
         {/* Placeholder 2 */}
         <div className="bg-gray-700 p-2 rounded">Video Demo</div>
-        {/* Placeholder 3 */}
-        <div className="bg-gray-700 p-2 rounded">PlaceHolder</div>
+        {/* Project Page Link */}
+        <a
+          href="https://emanlapaz.github.io/NeoLandingPage/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-700 p-2 rounded"
+        >
+          Project Page
+        </a>
         {/* Placeholder 4 */}
         <div className="bg-gray-700 p-2 rounded">PlaceHolder</div>
       </div>
