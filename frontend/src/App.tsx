7384fe00d4fcbb29a31405491c8d2bf7dfdf88ec
emanlapaz <eugeniojrmanlapaz@gmail.com>
2024-03-18 13:51:44 +0000

    uipdated app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
-import LoginPage from "./pages/LogIn";
+import LoginPage from "./pages/Login"; //log in on github
 import About from "./pages/About";
 import AccountSetting from "./pages/AccountSetting";
 import SignUp from "./pages/SignUp";
