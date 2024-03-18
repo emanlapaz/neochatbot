24baa4b84203b7ea1fd51d8561404e5e3dec0293
emanlapaz <eugeniojrmanlapaz@gmail.com>
2024-03-18 14:23:40 +0000

    firebaseconfig test

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  databaseURL: REACT_APP_DATABASE_URL,
+  apiKey: "AIzaSyCKjPPw2ARuL8GwpVUGKTjsmvsMcJr_DLk",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "neo-chatbot-e6c8c",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  databaseURL: "https://neo-chatbot-e6c8c-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 // Initialize Firebase
